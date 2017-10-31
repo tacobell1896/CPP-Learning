2b6ad4aa3dc286868584b8a02f72b65ad0c18f82
tacobell1896 <tbell.swd@gmail.com>
2017-10-31 00:43:11 +0000

    added another friend name, started the if statement.

--- LetterGenerator.cpp
+++ LetterGenerator.cpp
@@ -3,6 +3,8 @@
 
 int main() {
 	string first_name, friend_name;
+	char friend_sex;
+	friend_sex = 0;
 	cout << "What is your first name?\n";
 	cin >> first_name;
 	cout << "What is your friend's name?\n";
@@ -11,4 +13,14 @@ int main() {
 	cout << "Dear " + friend_name + ",\n"
 	+ "	Hey its your friend " + first_name
 	+ " How are you? I am fine. Have fun in Columbus, we really miss you!\n";
+	cout << "Please enter another friend's name.\n";
+	cin >> friend_name;
+	cout << "Have you seen " + friend_name + " lately?\n";
+	cout << "What gender are you? M/W\n";
+	cin >> friend_sex;
+	if (friend_sex = "m")
+		cout << "If you see " + friend_name + " please ask him to call me.\n";
+	else
+		cout << "If you see " + friend_name + " please ask her to call me.\n";
+
 }
